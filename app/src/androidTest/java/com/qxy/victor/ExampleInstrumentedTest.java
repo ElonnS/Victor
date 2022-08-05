86d19da2841c6163d60e765673082acc3e998be3
Elon Su <elonnsu@gmail.com>
2022-08-05 19:30:48 -0300

    Template Code & Initial Settings

--- app/src/androidTest/java/com/qxy/victor/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/qxy/victor/ExampleInstrumentedTest.java
@@ -2,8 +2,9 @@ package com.qxy.victor;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
